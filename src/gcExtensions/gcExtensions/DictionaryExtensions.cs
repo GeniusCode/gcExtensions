d5f07694fbb387373c2acfd3c9de0e559222c436
Jeremiah Redekop <jeremiah@geniuscode.net>
2012-01-11 15:50:21 -0800

    initial drop

--- src/gcExtensions/gcExtensions/DictionaryExtensions.cs
+++ src/gcExtensions/gcExtensions/DictionaryExtensions.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace gcExtensions
 {
-    public class Class1
+    public static class DictionaryExtensions
     {
+        public static TValue GetOrCreateValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> createDelegate)
+        {
+            bool wasCreated;
+            return GetOrCreateValue(dictionary, key, createDelegate, out wasCreated);
+        }
+
+        public static TValue GetOrCreateValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> createDelegate, out bool wasCreated)
+        {
+            TValue output;
+            if (dictionary.TryGetValue(key, out output))
+            {
+                wasCreated = false;
+                return output;
+            }
+
+            var result = createDelegate();
+            dictionary.Add(key, result);
+            wasCreated = true;
+            return result;
+        }
     }
 }
